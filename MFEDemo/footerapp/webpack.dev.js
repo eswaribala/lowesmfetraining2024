f43ce39a06b4ccea5bafc543282b7f76bcc586c1
Parameswari Bala <parameswaribala@gmail.com>
2024-10-08 10:59:10 +0530

    updated

---
+++ MFEDemo/footerapp/webpack.dev.js
@@ -0,0 +1,31 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
+const ModuleFederationPlugin=require('webpack/lib/container/ModuleFederationPlugin')
+const packageJson=require('./package.json')
+const devConfig = {
+  mode: 'development',
+  devServer: {
+    port: 3001,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name:'component1',
+      filename:'remoteEntry.js',
+      exposes:{
+        './Component1':'./src/bootstrap.js'
+      },
+      shared:packageJson.dependencies
+
+
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
