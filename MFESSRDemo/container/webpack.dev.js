afdbc4b9be7cb82dac01745d85002cbe1daf3b90
Parameswari Bala <parameswaribala@gmail.com>
2024-10-08 14:52:36 +0530

    updated

--- MFESSRDemo/container/webpack.dev.js
+++ MFESSRDemo/container/webpack.dev.js
@@ -2,30 +2,33 @@ const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin=require('webpack/lib/container/ModuleFederationPlugin')
-const packageJson=require('./package.json')
+const LoadablePlugin = require("@loadable/webpack-plugin");
+
 const devConfig = {
   mode: 'development',
   devServer: {
-    port: 3002,
+    port: 3000,
     historyApiFallback: {
       index: 'index.html',
     },
   },
   plugins: [
     new ModuleFederationPlugin({
-      name:'bodyComponent',
-      filename:'remoteEntry.js',
-      exposes:{
-        './BodyComponent':'./src/bootstrap.js'
-      },
-      shared:packageJson.dependencies
-
-
+      name: 'container',
+      remotes:{
+        headerComponent:'headerComponent@http://localhost:3001/remoteEntry.js',
+        bodyComponent:'bodyComponent@http://localhost:3002/remoteEntry.js',
+        footerComponent:'footerComponent@http://localhost:3003/remoteEntry.js'
+      }
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
+
+    new LoadablePlugin({ filename: 'loadable-stats.json' }), // Add this plugin to generate the file
+
   ],
+
 };
 
 module.exports = merge(commonConfig, devConfig);
