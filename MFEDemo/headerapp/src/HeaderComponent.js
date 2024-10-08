195c1dcedb55a8b7958b9ace693adb59f0c93c51
Parameswari Bala <parameswaribala@gmail.com>
2024-10-08 11:06:40 +0530

    updated

---
+++ MFEDemo/headerapp/src/HeaderComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const headerComponent=()=> {
+    return (
+        <div>
+          <h1>Lowes Services, Bangalore</h1>
+        </div>
+    )
+}
+
+export default headerComponent;
