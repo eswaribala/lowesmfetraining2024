456ab2c289a2973b1866cb8d846efd6caee8b333
Parameswari Bala <parameswaribala@gmail.com>
2024-10-08 15:06:11 +0530

    updated

---
+++ MFESSRDemo/container/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import { ChunkExtractor } from '@loadable/server';
+import path from 'path';
+import React from 'react';
+import { renderToPipeableStream } from 'react-dom/server';
+import App from './src/App';
+import loadable from '@loadable/component';
+
+//Dynamically load the MicroApp from the remote module
+//const Component1 = loadable(() => import('component1/Component1')); // This must match the 'exposes' path
+
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    const statsFile = path.resolve(__dirname, 'dist/loadable-stats.json');
+    const extractor = new ChunkExtractor({ statsFile });
+
+    const { pipe } = renderToPipeableStream(extractor.collectChunks(<App />), {
+        onShellReady() {
+            res.statusCode = 200;
+            res.setHeader('Content-type', 'text/html');
+            pipe(res);
+        },
+    });
+});
+
+app.listen(3004, () => console.log('SSR server running on http://localhost:3004'));
+
+
